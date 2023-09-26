7f8e3889c6f67afa910721afe66aaaa0a8e8f20e
Rplus <rplus.tw@gmail.com>
2023-09-26 18:23:16 +0800

    update menu
    add image proxy

--- extension/context.js
+++ extension/context.js
@@ -17,51 +17,51 @@
 // }
 
 var parent = chrome.contextMenus.create({
-  'title': '1. My context menus',
+  'title': '3. My context menus',
   'contexts': ['page', 'link']
 });
 
-let moveTab = (dir = 1) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (!tabs) { return; }
-    let tab = tabs[0];
-    let newIndex = Math.max(0, tab.index + dir);
-    chrome.tabs.move(tab.id, { index: newIndex });
-  });
-};
+// let moveTab = (dir = 1) => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     if (!tabs) { return; }
+//     let tab = tabs[0];
+//     let newIndex = Math.max(0, tab.index + dir);
+//     chrome.tabs.move(tab.id, { index: newIndex });
+//   });
+// };
 
-chrome.commands.onCommand.addListener((command) => {
-  switch (command) {
-    case 'move-tab-left':
-      moveTab(-1);
-      break;
-    case 'move-tab-right':
-      moveTab(1);
-      break;
-    default:
-      break;
-  };
-});
+// chrome.commands.onCommand.addListener((command) => {
+//   switch (command) {
+//     case 'move-tab-left':
+//       moveTab(-1);
+//       break;
+//     case 'move-tab-right':
+//       moveTab(1);
+//       break;
+//     default:
+//       break;
+//   };
+// });
+
+// chrome.contextMenus.create({
+//   'title': '`. get Twitter text',
+//   'contexts': ['page', 'link'],
+//   'parentId': parent,
+//   'onclick': (info, tab) => {
+//     let url = tab.url.split('?utm')[0];
+
+//     chrome.tabs.create({'url': `data:text/html;charset=utf-8,<meta charset="utf-8"><textarea style="font-size: 3em; width: 50vw; height: 50vh;">
+// ${tab.title}%0A
+// by ${new Date().toJSON().split('T')[0]}%0A
+// ${url}%0A
+// #f2etw
+// </textarea>`,
+//       'index': tab.index + 1});
+//   }
+// });
 
 chrome.contextMenus.create({
-  'title': '`. get Twitter text',
-  'contexts': ['page', 'link'],
-  'parentId': parent,
-  'onclick': (info, tab) => {
-    let url = tab.url.split('?utm')[0];
-
-    chrome.tabs.create({'url': `data:text/html;charset=utf-8,<meta charset="utf-8"><textarea style="font-size: 3em; width: 50vw; height: 50vh;">
-${tab.title}%0A
-by ${new Date().toJSON().split('T')[0]}%0A
-${url}%0A
-#f2etw
-</textarea>`,
-      'index': tab.index + 1});
-  }
-});
-
-chrome.contextMenus.create({
-  'title': '1. 繁',
+  'title': 'z. 繁',
   'contexts': ['page', 'link'],
   'parentId': parent,
   'onclick': function (info, tab) {
@@ -70,25 +70,25 @@ chrome.contextMenus.create({
   }
 });
 
-chrome.contextMenus.create({
-  'title': '2. FB',
-  'contexts': ['page', 'link'],
-  'parentId': parent,
-  'onclick': function (info, tab) {
-    var url = info.linkUrl || info.pageUrl;
-    if (/www\.facebook\.com/.test(url)) {
-      window.open(url.replace(/www\.facebook\.com/, 'm.facebook.com'));
-    } else if (/m\.facebook\.com/.test(url)) {
-      var personalUrl = url.match(/story_fbid=(\d+)&id=(\d+)/);
+// chrome.contextMenus.create({
+//   'title': '2. FB',
+//   'contexts': ['page', 'link'],
+//   'parentId': parent,
+//   'onclick': function (info, tab) {
+//     var url = info.linkUrl || info.pageUrl;
+//     if (/www\.facebook\.com/.test(url)) {
+//       window.open(url.replace(/www\.facebook\.com/, 'm.facebook.com'));
+//     } else if (/m\.facebook\.com/.test(url)) {
+//       var personalUrl = url.match(/story_fbid=(\d+)&id=(\d+)/);
 
-      if (personalUrl) {
-        window.open('https://www.facebook.com/' + personalUrl[2] + '/posts/' + personalUrl[1]);
-      } else {
-        window.open(url.replace(/m\.facebook\.com/, 'www.facebook.com'));
-      }
-    }
-  }
-});
+//       if (personalUrl) {
+//         window.open('https://www.facebook.com/' + personalUrl[2] + '/posts/' + personalUrl[1]);
+//       } else {
+//         window.open(url.replace(/m\.facebook\.com/, 'www.facebook.com'));
+//       }
+//     }
+//   }
+// });
 
 chrome.contextMenus.create({
   'title': '3. open iframe',
@@ -99,52 +99,43 @@ chrome.contextMenus.create({
   }
 });
 
+// chrome.contextMenus.create({
+//   'title': '4. Twitter',
+//   'contexts': ['page', 'link'],
+//   'parentId': parent,
+//   'onclick': function (info, tab) {
+//     var url = info.linkUrl || info.pageUrl;
+//     if (/\/\/twitter\.com/.test(url)) {
+//       window.open(url.replace(/twitter\.com/, 'mobile.twitter.com'));
+//     } else if (/mobile\.twitter\.com/.test(url)) {
+//       window.open(url.replace(/mobile\.twitter\.com/, 'twitter.com'));
+//     }
+//   }
+// });
+
 chrome.contextMenus.create({
-  'title': '4. Twitter',
-  'contexts': ['page', 'link'],
-  'parentId': parent,
+  'title': '1 open img by proxy',
+  'contexts': ['image'],
   'onclick': function (info, tab) {
-    var url = info.linkUrl || info.pageUrl;
-    if (/\/\/twitter\.com/.test(url)) {
-      window.open(url.replace(/twitter\.com/, 'mobile.twitter.com'));
-    } else if (/mobile\.twitter\.com/.test(url)) {
-      window.open(url.replace(/mobile\.twitter\.com/, 'twitter.com'));
-    }
+    let url = `https://proxy.duckduckgo.com/iu/?u=${info.srcUrl}`;
+    window.open(url);
   }
 });
 
 chrome.contextMenus.create({
-  'title': 'open img',
+  'title': '2 open img by wsrv',
   'contexts': ['image'],
   'onclick': function (info, tab) {
-    function convertImgToBase64 (url, callback, outputFormat) {
-      var canvas = document.createElement('CANVAS');
-      var ctx = canvas.getContext('2d');
-      var img = new Image();
-      img.crossOrigin = 'Anonymous';
-      img.onload = function () {
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        var dataURL = canvas.toDataURL();
-        callback.call(this, dataURL);
-        canvas = null;
-      };
-      img.src = url;
-    }
-
-    var imageUrl = info.srcUrl;
-    convertImgToBase64(imageUrl, function (base64Img) {
-      var x = /\xff\xc2/.test(base64Img);
-      console.log(base64Img, x);
-    });
+    let url = `https://proxy.duckduckgo.com/iu/?u=${info.srcUrl}`;
+    url = `https://wsrv.nl/?&w=2000&h=2000&we&il&output=webp&url=${encodeURIComponent(url)}`;
+    window.open(url);
   }
 });
 
 chrome.contextMenus.create({
   'title': '1. 譯: %s',
   'contexts': ['selection'],
-  // 'parentId': parent,
+  'parentId': parent,
   'onclick': function (info, tab) {
     console.log(info, tab);
     window.open('http://cdict.net/?q=' + info.selectionText, 'CDICT');
